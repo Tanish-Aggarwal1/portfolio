e2180a89ff5388f9781fe8cb2f5216eddeb735cb
Tanish Aggarwal <atanish6@gmail.com>
2026-07-23 22:42:28 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component, Input } from '@angular/core';
+import { Personal, Projects } from './classes';
+import content from "../assets/data/content.json";
+import projectData from "../assets/data/project.json";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'portfolio_source';
+  title = 'portfolio';
+  checked!:boolean;
+
+  myData:Personal = content.myData;
+  projects:Projects[] = projectData.projects;
+  aboutMe = content.about;
+  footer = content.footer;
+
+  
+
+  
 }
